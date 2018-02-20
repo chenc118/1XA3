6d8c7b59988df05038bab3116dc0a2bcb141d887
chenc118 <chenc118@mcmaster.ca>
2018-02-19 22:58:32 -0500

    Finished diff stuff
    -did it cause it prints things neatly as opposed to the mess that comes
    from the default diff command.
    -also mimics somewhat git desktop, cannot mimic the expanding part w/o
    implementing the html stuff and figuring how to use javascript to do
    that

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,5 +1,7 @@
 #!/git-bash.exe
 
+#Potential feature turning some of this stuff into HTML if -report flag in args?
+
 # random variable, not sure if I'll use it
 UPTODATE=$(git pull)
 
@@ -43,7 +45,7 @@ $( echo "$Status" | sed "$STATUSCLEAN;/Changes to be committed:/d;/Changes not s
 #---- Find uncommitted changes II ----
 #uses git diff prints in a cleaner style than default with lines numbered and all that
 
-$Diff=$(git diff)
+Diff=$( git diff )
 
 $(echo "Current git diff:" >> changes.log)
 
@@ -58,38 +60,72 @@ ACodeCount="0"
 #Numbers to count code line
 RNum="0"
 ANum="0"
-
+#padding altered from the following https://stackoverflow.com/questions/4409399/padding-characters-in-printf
+#literally just a bunch of spaces
+Pad=$(printf '%0.1s' " "{1..60})
+#length to pad the numbers
+PadL=6
 
 #iterate over the lines of the diff stuff
 while read -r line; do
 	#non code lines either of 5 types
-	if [ $Code == "False" ]
+	if [ "$Code" == "False" ]
 	then
 		#use multi sed command to get what exactly the non code line is
-		Status=$(echo $line | sed -e "s/^diff --git.*/1/;s/^index.*/2/;s/^---.*/3/;s/^+++.*/4/;s/^@@.*/5/;s/[^1-5]/6/;s/\(.\).*/\1/")
+		Status=$(echo "$line" | sed -e "s/^diff --git.*/1/;s/^index.*/2/;s/^---.*/3/;s/^+++.*/4/;s/^@@.*/5/;s/[^1-5]/6/;s/\(.\).*/\1/;s/^$/6/")
 		#1 = first line 2 = index objects 3 = --- 4 = +++ 5 = @@ aka start of code 6 = mode or something, too difficult to match easily
-		if [ $Status -eq 5 ]
+		if [ "$Status" -eq "5" ]
 		then
 			Code="True"
-			RNum=$(echo $line | sed -e "s/^@@ -\([0-9]*\),.*/\1/")
-			ANum=$(echo $line | sed -e "s/^@@ -[0-9]*,[0-9]* +\([0-9]*\),.*/\1/")
-			RCodeCount=$(echo $line | sed -e "s/^@@ -[0-9]*,\([0-9]*\) .*/\1/")
-			ACodeCount=$(echo $line | sed -e "s/^@@ -[0-9]*,[0-9]* +[0-9]*,\([0-9]*\).*/\1/")
+			RNum=$(echo "$line" | sed -e "s/^@@ -\([0-9]*\),.*/\1/")
+			ANum=$(echo "$line" | sed -e "s/^@@ -[0-9]*,[0-9]* +\([0-9]*\),.*/\1/")
+			RCodeCount=$(echo "$line" | sed -e "s/^@@ -[0-9]*,\([0-9]*\) .*/\1/")
+			ACodeCount=$(echo "$line" | sed -e "s/^@@ -[0-9]*,[0-9]* +[0-9]*,\([0-9]*\).*/\1/")
 			#TODO print stuff to changelog
 
-		elif [$Status -eq 4]
+		elif [ "$Status" -eq "4" ]
 		then
 			# dump the file name into the changelog
-			$(echo $line | sed -e "s/^+++ b\//File diff: /" >> changes.log)
+			$(echo >> changes.log)
+			$(echo "$line" | sed -e "s/^+++ b\//File diff: /" >> changes.log)
 		fi
-		echo $Status
 
 	elif [ $Code == "True" ]
 	then
-		if [ $CodeCount -eq 0 ]
+		if [ $ACodeCount -eq 0 -a $RCodeCount -eq 0 ]
 		then
 			Code="False"
 			#TODO everything else and determine what the code count must be equal to in order to switch out of this state
+		else
+			#Print lines, numbering etc
+			Start=$(echo $line | sed -e "s/\(.\).*/\1/")
+			if [ "$Start" = "+" ]
+			then
+				PadA=$(( $PadL - ${#ANum} ))
+				$(printf '%*.*s%s|%*.*s%s|%s' 0 "$PadL" "$Pad" "" 0 "$PadA" "$Pad" "$ANum" "$line" >> changes.log)
+				ANum=$(( $ANum + 1 ))
+				ACodeCount=$(( $ACodeCount - 1 ))
+			elif [ "$Start" = "-" ]
+			then
+				PadR=$(( $PadL - ${#RNum} ))
+				$(printf '%*.*s%s|%*.*s%s|%s' 0 "$PadR" "$Pad" "$RNum" 0 "$PadL" "$Pad" "" "$line" >> changes.log)
+				RNum=$(( $RNum + 1 ))
+				RCodeCount=$(( $RCodeCount - 1 ))
+			else
+				PadR=$(( $PadL - ${#RNum} ))
+				PadA=$(( $PadL - ${#ANum} ))
+				$(printf '%*.*s%s|%*.*s%s|%s' 0 "$PadR" "$Pad" "$RNum" 0 "$PadA" "$Pad" "$ANum" "$line" >> changes.log)
+				#increment decrement stuff
+				RNum=$(( $RNum + 1 ))
+				ANum=$(( $ANum + 1 ))
+				RCodeCount=$(( $RCodeCount - 1 ))
+				ACodeCount=$(( $ACodeCount - 1 ))
+
+			fi
+			
+			$(echo >> changes.log) #to add newline
+
+
 		fi
 
 	fi
--- Assign1/testscript.sh
+++ Assign1/testscript.sh
@@ -1,4 +1,4 @@
 #!/git-bash.exe
 
-Status=$(sed -e "s/^diff --git.*/1/;s/^index.*/2/;s/^---.*/3/;s/^+++.*/4/;s/^@@.*/5/;s/[^1-5]/6/;s/\(.\).*/\1/" < test.txt)
+Status=$(echo >> changes.log)
 echo $Status
