f453cbbb8c366b4be0713c4535ea9db67ba5d337
chenc118 <chenc118@mcmaster.ca>
2018-03-18 16:51:07 -0400

    A slew of improvements
    -added a start menu with a few custom selectors,(very minimal at the
    moment)
    -added grid lines
    -centered on the player and zooms to an appropriate level
    -fully implemented custom images which you can use for your player
    character
    -TODO: a bunch of things

--- Assign2/agar.elm
+++ Assign2/agar.elm
@@ -1,13 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (onClick,onInput)
+import Html.Attributes exposing (..)
 import Platform.Cmd as Cmd
 import Platform.Sub as Sub
 import Keyboard as Key
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Random exposing (..)
-import ImgDim exposing (..)
 
 --a highly simplified version of agar.io, not going to bother with the time needed to make the background scroll and other stuff
 
@@ -18,16 +19,29 @@ type alias Feed = {x:Int,y:Int,value:Int,color:String}
 type Either a b = RS a | LS b -- lazy implementation of Haskell's Either
 
 type alias Display = Either PImage String -- can be one or the other, need to extract on display
-type alias PImage = {source:String, x: Int, y:Int} -- basic stuff needed to model the player image
+type alias PImage = {source:String} -- basic stuff needed to model the player image
 
 type alias Model = {x:Int, y:Int,
-                    feed: FeedBit,size:Float,display:Display} -- list of records representing the different dots etc
+                    name:String,
+                    feed: FeedBit,
+                    size:Float,
+                    display:Display,
+                    inGame:Bool} -- list of records representing the different dots etc
+type alias DUpdate = Either String String --either holds info on whether it came for URL input or radio button input
 
-type Msg = KeyMsg Key.KeyCode | RandResult (Int,Int) 
+type Msg = KeyMsg Key.KeyCode | RandResult (Int,Int) | DispUpdate DUpdate |NameUpdate String | StartG
 
 
 init : (Model, Cmd.Cmd Msg)
-init = ({x=round<| (toFloat svWidth)/2,y=round <| (toFloat svHeight)/2,feed=[],size=25,display = RS {x = 512,y=512, source = "https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._SY355_.jpg"}}, Cmd.none)
+init = ({x=round<| (toFloat svWidth)/2,
+        y=round <| (toFloat svHeight)/2,
+        name="unknown",
+        feed=[],
+        size=25,-- set size to a really big number to see - infinity
+        display = LS "red",
+        inGame=False
+    }
+    , Cmd.none)
 
 
 extractMod : (Model, Cmd.Cmd Msg) -> Model
@@ -38,7 +52,7 @@ resetGame : Model -> Model
 resetGame model = let 
         initial = extractMod init
 
-    in {model | x=initial.x, y = initial.y, feed = initial.feed, size = initial.size}
+    in {model | x=initial.x, y = initial.y, feed = initial.feed, size = initial.size,inGame=False}
 
 
 
@@ -52,6 +66,10 @@ boundsCheck bounds pos rad = if pos>=bounds+rad then
                             pos + bounds+(2*rad)
                         else
                             pos
+
+genViewBox : Int -> (Int,Int)->Svg.Attribute msg
+genViewBox rad (x,y)= Svg.Attributes.viewBox ((x-rad*4|>toString)++" "++(y-rad*4|>toString)++" "++(rad*8|>toString)++" "++(rad*8|>toString))
+
 svWidth : Int 
 svWidth = 1300
 
@@ -78,6 +96,32 @@ buildFeeds: FeedBit -> List (Svg.Svg msg)
 buildFeeds feed = case feed of -- literally just a bunch of tiny circles
                     (f::fs) -> (Svg.circle [cx (toString f.x),cy (toString f.y), r (toString <| mr f.value), fill f.color][])::(buildFeeds fs)
                     []      -> []-- return empty list on list end
+
+
+
+drawLines : List (Svg.Svg msg)
+drawLines = (drawXLines 10 svHeight 0 svWidth [])++(drawYLines 10 svWidth 0 svHeight [])
+
+
+drawXLines : Int-> Int-> Int -> Int -> List (Svg.Svg msg) -> List (Svg.Svg msg)
+drawXLines inc len lx total lines= if lx>total then lines
+                        else drawXLines inc len (lx+inc) total 
+                                (line[x1 (Basics.toString lx),
+                                    x2 (Basics.toString lx), 
+                                    y1 "0",
+                                    y2 (Basics.toString len), 
+                                    Svg.Attributes.strokeWidth "1px", 
+                                    Svg.Attributes.stroke "lightgrey"][]::lines)
+
+drawYLines : Int-> Int-> Int -> Int -> List (Svg.Svg msg) -> List (Svg.Svg msg)
+drawYLines inc len ly total lines= if ly>total then lines
+                        else drawYLines inc len (ly+inc) total 
+                                (line[y1 (Basics.toString ly),
+                                y2 (Basics.toString ly), 
+                                x1 "0",
+                                x2 (Basics.toString len), 
+                                Svg.Attributes.strokeWidth "1px", 
+                                Svg.Attributes.stroke "lightgrey"][]::lines)
 
 --update model consuming feeds that overlap with circle
 testConsume: Model -> Model
@@ -147,6 +191,12 @@ genColor x = let
                 12 -> "brown"
                 _ -> "white" -- the devil's dot, aka it can't be seen
 
+
+updatePlayerDisplay: DUpdate -> Display -> Display
+updatePlayerDisplay du model = case du of 
+                    (LS s) -> LS s
+                    (RS s) -> RS {source=s}
+
 update : Msg -> Model -> (Model, Cmd.Cmd Msg)
 update msg model = case msg of --wasd
             (KeyMsg 87) -> (testConsume {model|y = bChecky (model.y-incNum) (round <| model.size)},genRand)
@@ -160,6 +210,9 @@ update msg model = case msg of --wasd
             (KeyMsg 39) -> (testConsume {model|x = bCheckx (model.x+incNum) (round <| model.size)},genRand)
             --reset the game if escape key is pressed
             (KeyMsg 27) -> (resetGame model, Cmd.none)
+            (DispUpdate u) -> ({model | display = updatePlayerDisplay u model.display},Cmd.none)
+            (NameUpdate s) -> ({model | name = s},Cmd.none)
+            (StartG)       -> ({model | inGame = True},Cmd.none)
             (RandResult (a,b)) -> if (List.length model.feed < 100)  --limit so there's an upper bound to the size
                                 then (genFeed a b model,Cmd.none) 
                                 else (model,Cmd.none)
@@ -168,8 +221,8 @@ update msg model = case msg of --wasd
 
 genRand = generate RandResult (Random.pair (int 1 10 ) (int 1 (svWidth*svHeight)))
 
-view : Model -> Html Msg
-view model =
+gameView : Model -> Html Msg -- view used for when the game is being played
+gameView model = 
     let 
         posX = (toString model.x)
         posY = (toString model.y)
@@ -179,26 +232,59 @@ view model =
             (RS _) -> "url(#player)" -- use the pattern image
         pImage = case model.display of
             (LS _) -> image [][]
-            (RS r) -> image [x "0%",y "0%",height (toString r.y),width (toString r.x),Svg.Attributes.xlinkHref r.source][]
-        vBox = case model.display of
-            (LS _) -> viewBox "0 0 512 512"
-            (RS r) -> viewBox ("0 0 "++(r.x |>toString)++" "++(r.y |> toString))
-
+            (RS r) -> image [x "0%",y "0%",Svg.Attributes.height "5000",Svg.Attributes.width "5000",Svg.Attributes.xlinkHref r.source][]
+        vBox = genViewBox (mr <| round <| model.size) (model.x,model.y)
+        gridlines = drawLines
     in div[][
-        svg[Svg.Attributes.width (toString svWidth),Svg.Attributes.height (toString svHeight)](
+        svg[Svg.Attributes.width (toString svWidth),Svg.Attributes.height (toString svHeight),vBox](
             [--image in svg based on this https://stackoverflow.com/questions/29442833/svg-image-inside-circle
             defs [][
-                pattern[id "player", x "0%", y "0%", height "100%", width "100%", vBox][
-                    pImage
+                Svg.pattern[Svg.Attributes.id "player", x "0%", y "0%", Svg.Attributes.height "100%", Svg.Attributes.width "100%", viewBox "0 0 5000 5000"][
+                    pImage-- um... somehow if you set viewbox = image height/width it automatically sizes it for you... *throws away a bunch of code that hasn't been coded yet*
                     ]
                 ]
-            ]++feeds++[Svg.circle [cx posX,cy posY, r (toString <| mr <| round model.size),fill pfill, stroke "black", Svg.Attributes.strokeWidth "1px"] []])
+            ]++gridlines++feeds++[Svg.circle [cx posX,cy posY, r (toString <| mr <| round model.size),fill pfill, stroke "black", Svg.Attributes.strokeWidth "1px"] []])
         ,div[][Html.text ("Score "++(toString <|round<| model.size-25))]
-        ,div[][Html.text "To play use the arrow keys to move the red circle around. The goal is to consume the small dots and grow as much as possible, if you get over 2236 you win!"]
         ]
 
+
+
+radioStyle = Html.Attributes.style[("clear","both"),("margin","auto"),("text-align","unset")]
+
+radioButton : String -> Html Msg
+radioButton color = label[radioStyle][input[Html.Attributes.type_ "radio",Html.Attributes.name "colorPick",onClick (DispUpdate (LS color))][],Html.text color]
+
+preView : Model -> Html Msg
+preView model = 
+    let 
+        n = ""
+    in div [Html.Attributes.style[("margin","auto"),("text-align","center"),("height","80%"),("width","80%"),("background-color","lightgrey")]][
+        div[][Html.text "Enter your name"]
+        ,div[][Html.input [Html.Attributes.placeholder "unknown name", Html.Events.onInput (\inp -> NameUpdate inp),radioStyle][]]
+        ,div [][Html.text "Enter the url for the image or choose a color from below"]
+        ,div[][input [Html.Attributes.placeholder "Input Image URL", onInput (\inp ->DispUpdate (RS inp)),radioStyle][]]
+        ,div[][--div holding a bunch of stuff relating to the color
+            radioButton "red",
+            radioButton "blue",
+            radioButton "green"
+            ]
+        ,div[][button[Html.Events.onClick StartG][Html.text "Start"]]
+        ,div[][
+            strong[][Html.text "Instructions:"]
+            ,p[][Html.text "Use the arrow keys or WASD to move the circle around"]
+            ,p[][Html.text "The goal is eat smaller dots and grow. The goal is to get to 2000 points"]
+            ]
+    ]
+
+view : Model -> Html Msg
+view model = case model.inGame of
+    True -> gameView model
+    False -> preView model
+
 subscriptions : Model -> Sub.Sub Msg
-subscriptions model = Key.downs KeyMsg
+subscriptions model = case model.inGame of
+    True -> Key.downs KeyMsg -- only need keys during game
+    False -> Sub.none --- listens to nothing due to nothing going on pre game
 
 main : Program Never Model Msg
 main = program
@@ -206,3 +292,5 @@ main = program
         view = view,
         update = update,
         subscriptions = subscriptions}
+
+--bookmark in case I need it to draw the border for custom img https://raw.githubusercontent.com/lokesh/color-thief/master/src/color-thief.js
