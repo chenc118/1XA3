b21b36b33ddeb0e450924c2a8fa4d2e149ad4509
chenc118 <chenc118@mcmaster.ca>
2018-04-08 21:10:36 -0400

    Indecisive stuffs
    Also added ln, might add a log base n x thing if I can figure out how to take their derivatives easily

--- Assign3/ExprType.hs
+++ Assign3/ExprType.hs
@@ -19,7 +19,8 @@ data Expr a = Add (Expr a) (Expr a) -- ^ Binary Addition
             | Cos (Expr a)
             | Sin (Expr a)
             | Log (Expr a)
-            | Exp (Expr a) (a) -- ^ Exponentiate a function using ^
+            | Ln (Expr a)            
+            | Exp (Expr a) (a) -- ^ Exponentiate a function using ^  -- really not sure how to approach this, TBD
             | Const a               -- ^ Wrap a constant value
             | Var String            -- ^ Wrap a variable identifier
             deriving Eq
@@ -33,6 +34,9 @@ getVars :: Expr a-> [String]
 getVars (Add e1 e2)  = (getVars e1) `union` (getVars e2)
 getVars (Mult e1 e2) = (getVars e1) `union` (getVars e2)
 getVars (Cos e1)     = getVars e1
+getVars (Sin e1)     = getVars e1
+getVars (Log e1)     = getVars e1
+getVars (Ln e1)      = getVars e1
 getVars (Const _)    = []
 getVars (Var s)      = [s]
 
